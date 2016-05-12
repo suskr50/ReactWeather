69a8969a662451e9dc086dec59a6075f237433b9
Susan <susanschrum@mgail.com>
2016-05-12 09:05:46 -0400

    Prep repo for Herolu

--- server.js
+++ server.js
@@ -1,20 +1,19 @@
 var express = require('express');
 
-	//Create our app
-	var app = express();
-	const port = process.env.PORT || 3000;
+// Create our app
+var app = express();
+const PORT = process.env.PORT || 3000;
 
-app.use(function(req, res, next){
-	if (req.headers.) {
-
-	}else {
-
-	}
-
+app.use(function (req, res, next){
+  if (req.headers['x-forwarded-proto'] === 'http') {
+    next();
+  } else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
 });
 
-	app.use (express.static('public'));
+app.use(express.static('public'));
 
-	app.listen(port,function() {
-		console.log('Express sever is up on port  ' + port);
-	})
+app.listen(PORT, function () {
+  console.log('Express server is up on port ' + PORT);
+});
