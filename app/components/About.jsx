696a1582e5947fad5806caf8bd1ca04c1bc02c82
Susan <susanschrum@mgail.com>
2016-05-12 12:00:02 -0400

    Style about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -4,8 +4,9 @@ var React = require('react');
 var About = (props) => {
 	return(
 		<div>
-			<h3>About </h3>
-			<p> This is a little about nothing</p>
+			<h1 className="text-center">About </h1>
+			<p > Complete React developers course</p>
+			<p> See the about page lessone to add more things about tools here later </p>
 		</div>
 		);
 }
