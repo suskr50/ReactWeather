959045788eb7dcba19ee2ef445bdf6f41961b023
Susan <susanschrum@mgail.com>
2016-05-14 15:26:50 -0400

    Added sass files

--- app/app.jsx
+++ app/app.jsx
@@ -9,7 +9,7 @@ var About = require('About');
 // Load foundation 
 require('style!css!foundation-sites/dist/foundation.min.css');
 
-require('style!css!applicationStyles');
+require('style!css!sass!applicationStyles');
 
 $(document).foundation();
 
