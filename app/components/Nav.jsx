2b62c48a81c3dc16c65476364e640cbddf2df6d4
Susan <susanschrum@mgail.com>
2016-05-14 16:14:54 -0400

    Wired search bar to actually search

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -6,36 +6,42 @@ var {IndexLink} = require('react-router');
 
 var Nav = React.createClass({
   onSearch: function (e) {
-      e.preventDefault();
-      alert('Not yet wired up!');
-  },
-  render: function () {
-	return(
-		<div className="top-bar">
-			<div className="top-bar-left">
-				<ul className="menu">
-					<li className="menu-text">React Weather App</li>
-					<li><IndexLink to ="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink></li>
-					<li><Link to ="/about" activeClassName="active"   activeStyle={{fontWeight: 'bold'}}>About</Link></li>
-					<li><Link to ="/example" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Example</Link></li>
-				</ul>
-			</div>
-			<div className="top-bar-right">
-			<form onSubmit = {this.onSearch}>
-			<ul className="menu">
-              <li>
-                <input type="search" placeholder="Search weather by city"/>
-              </li>
-              <li>
-                <input type="submit" className="button" value="Get Weather"/>
-              </li>
-            </ul>
-			</form>
-			</div>
-		</div>
-		
-    );
-  }
+    e.preventDefault();
+    
+    var location = this.refs.city.value;
+    var encodedLocation = encodeURIComponent(location);
+
+    this.refs.city.value = '';
+    window.location.hash = '#/' + '?location=' + encodedLocation;
+
+},
+render: function () {
+return(
+<div className="top-bar">
+  <div className="top-bar-left">
+    <ul className="menu">
+      <li className="menu-text">React Weather App</li>
+      <li><IndexLink to ="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink></li>
+      <li><Link to ="/about" activeClassName="active"   activeStyle={{fontWeight: 'bold'}}>About</Link></li>
+      <li><Link to ="/example" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Example</Link></li>
+    </ul>
+  </div>
+  <div className="top-bar-right">
+    <form onSubmit = {this.onSearch}>
+      <ul className="menu">
+        <li>
+          <input type="search" ref="city" placeholder="Search weather by city"/>
+        </li>
+        <li>
+          <input type="submit" className="button" value="Get Weather"/>
+        </li>
+      </ul>
+    </form>
+  </div>
+</div>
+
+);
+}
 });
 
 module.exports = Nav;
